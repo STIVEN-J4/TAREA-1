d8806bd9aa5d32398cfee9d866ea236083cb3bbc
STIVEN-J4 <65369672+STIVEN-J4@users.noreply.github.com>
2020-05-14 21:30:06 -0500

    creacion del archivo imprimir un numero

---
+++ Imprimir un numero.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+                 /* ingresar y lea un numero digito e imprima en pantalla*/
+                 int numero;
+                 printf("ingrese un numero digito:");
+                 scanf("%d",&numero);
+                 printf("%d",numero);
+}
